529e29e27a5c942f30d8acd39fede2713020afe8
Aimen <iqbal001001@outlook.com>
2021-08-30 23:44:26 +1000

    refactor extension

--- Calculator.Web/ExtensionMethod/StringExtension.cs
+++ Calculator.Web/ExtensionMethod/StringExtension.cs
@@ -11,9 +11,7 @@ namespace Calculator.Web.Extension
         public static List<T> GetList<T>(this string s)
         {
             TypeConverter tc = TypeDescriptor.GetConverter(typeof(T));
-            IEnumerable<T> z = s.Replace(" ", "").Split(",").Select(x => (T) tc.ConvertFrom(x));
-            var y = z.ToList();
-            return y;
+            return s.Replace(" ", "").Split(",").Select(x => (T) tc.ConvertFrom(x)).ToList();
         }
     }
 }
